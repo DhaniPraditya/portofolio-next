77a97c9979c857f173c259dfa3945e418013d588
unknown <dhanipraditya@gmail.com>
2026-05-11 15:32:34 +0700

    style: font changed to plus jakarta sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-plus-jakarta-sans",
   subsets: ["latin"],
 });
 
@@ -25,12 +20,13 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} scroll-smooth`}
+      className={`${plusJakartaSans.variable} scroll-smooth`}
     >
-      <body className="bg-background text-foreground selection:bg-primary/30">
+      <body className="bg-background text-foreground selection:bg-primary/30 font-sans">
         {children}
       </body>
     </html>
   );
 }
+
 
